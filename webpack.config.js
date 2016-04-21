b35be3386aff5b010f907485da60a7abe0b0ed0a
sophiaxhe <sophiahxx@gmail.com>
2016-04-21 11:57:40 -0400

    add webpack and npm bundle installed

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {}
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [{test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+  ]
+},
+output: {
+  filename: "index_bundle.js",
+  path: __dirname + '/dist'
+},
+}
