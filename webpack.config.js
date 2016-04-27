d2719416cc66e545e88091be6deee87dd3e8df6b
sophiaxhe <sophiahxx@gmail.com>
2016-04-27 10:36:44 -0400

    added modules and submodules to user data

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-module.exports = {}
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
   entry: [
     './app/index.js'
   ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
   module: {
-    loaders: [{test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
-  ]
-},
-output: {
-  filename: "index_bundle.js",
-  path: __dirname + '/dist'
-},
-}
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
