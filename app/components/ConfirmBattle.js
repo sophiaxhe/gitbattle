e2f29095ad3c6151edde394cfec09f0311788520
sophiaxhe <sophiahxx@gmail.com>
2016-05-02 17:23:31 -0400

    added player info to battle routes using promises and axios

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,21 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+
+function puke(object){
+  return <pre> {JSON.stringify(object,null, ' ')}</pre>
+}
 
 function ConfirmBattle(props) {
   return props.loading === true
   ? <p> LOADING! </p>
-  : <p> CONFIRM BATTLE! </p>
-
+  : <div> CONFIRM BATTLE!: {puke(props)}</div>
 }
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+}
+
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes:{
@@ -11,17 +12,26 @@ var ConfirmBattleContainer = React.createClass({
       playerInfo: []
     }
   },
-  componentDidMount: function() {
+  componentDidMount: function () {
     var query = this.props.location.query;
-    // Fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
+  handleInitiateBattle: function(){
 
-  render: function(){
+  }
+  render: function () {
     return (
       <ConfirmBattle
-      isLoading={this.state.isLoading}
-      playersInfo={this.state.playersInfo}/>
-    );
+        isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
+        playersInfo={this.state.playersInfo} />
+    )
   }
 });
 
